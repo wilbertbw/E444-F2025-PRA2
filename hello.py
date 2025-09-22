2278e7f09995a90ca10bb7999061fe967cd5e524
wilbertbw <wilbertbwibowo@gmail.com>
2025-09-21 23:34:05 -0400

    added an input field for the UofT email (checks if input is an email), added saving email, alert for changing email, and displaying email

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -12,21 +12,26 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
   name = StringField('What is your name?', validators=[DataRequired()])
+  email = EmailField('What is your UofT email address?', validators=[DataRequired()])
   submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   form = NameForm()
-  
+
   if form.validate_on_submit():
     old_name = session.get('name')
+    old_email = session.get('email')
     if old_name is not None and old_name != form.name.data:
       flash("You have changed your name.")
+    if old_email is not None and old_email != form.email.data:
+      flash("You have changed your email.")
     
-    session['name'] = form.name.data  
+    session['name'] = form.name.data
+    session['email'] = form.email.data
     return redirect(url_for('index'))
   
-  return render_template('index.html', form=form, name=session.get('name'))
+  return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.errorhandler(404)
 def page_not_found(e):
