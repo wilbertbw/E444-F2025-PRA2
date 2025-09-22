18c86f12693c8a750d69e12052ac04c976e36c13
wilbertbw <wilbertbwibowo@gmail.com>
2025-09-21 23:13:51 -0400

    added an alert when user inputs a new name

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -17,7 +17,12 @@ class NameForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def index():
   form = NameForm()
+  
   if form.validate_on_submit():
+    old_name = session.get('name')
+    if old_name is not None and old_name != form.name.data:
+      flash("You have changed your name.")
+    
     session['name'] = form.name.data  
     return redirect(url_for('index'))
   
