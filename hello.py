43e20d3cae7cc5cb18219ce9c4ca9f4fde0ba89b
wilbertbw <wilbertbwibowo@gmail.com>
2025-09-21 22:59:17 -0400

    added redirect on user submission (so last request isn't post), saved inputted name to session variable

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -16,12 +16,12 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  name = None
   form = NameForm()
   if form.validate_on_submit():
-    name = form.name.data
-    form.name.data = ""
-  return render_template('index.html', form=form, name=name)
+    session['name'] = form.name.data  
+    return redirect(url_for('index'))
+  
+  return render_template('index.html', form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
