43a7c9d34917955f649a610be75de26a03a9e9b2
wilbertbw <wilbertbwibowo@gmail.com>
2025-09-21 22:45:27 -0400

    removed the /user/<name>/ route and user.html template, added a form for users to fill in their name, which is displayed after validation

--- hello.py
+++ hello.py
@@ -1,20 +1,27 @@
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask import Flask, render_template
-
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "ECE444 lab 2"
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def index():
-  return render_template('index.html', current_time=datetime.utcnow())
+class NameForm(FlaskForm):
+  name = StringField('What is your name?', validators=[DataRequired()])
+  submit = SubmitField('Submit')
 
-@app.route('/user/<name>')
-def user(name):
-  return render_template('user.html', name=name, current_time=datetime.utcnow())
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  name = None
+  form = NameForm()
+  if form.validate_on_submit():
+    name = form.name.data
+    form.name.data = ""
+  return render_template('index.html', form=form, name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
